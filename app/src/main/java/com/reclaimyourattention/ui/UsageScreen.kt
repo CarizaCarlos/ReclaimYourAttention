16cb105344d0f8e2b80c4ab71831845df5cb5388
CarlosAriza <carizacarlos@gmail.com>
2025-02-15 23:16:29 -0500

    Basic Idea for some Components on the Usage Screen UI

--- app/src/main/java/com/reclaimyourattention/ui/UsageScreen.kt
+++ app/src/main/java/com/reclaimyourattention/ui/UsageScreen.kt
@@ -1,7 +1,9 @@
 package com.reclaimyourattention.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -15,7 +17,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.navigation.NavController
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.reclaimyourattention.R
+import com.reclaimyourattention.ui.theme.ReclaimYourAttentionTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -37,6 +43,41 @@ fun UsageScreen(navController: NavController) {
             Button(onClick = { navController.navigate("tools") }) {
                 Text("Ir a Herramientas")
             }
+
+            // Info específica de Apps
+            appInfo("Youtube", 25)
+            appInfo("Instagram", 55)
         }
     }
+}
+
+@Composable
+@Preview
+fun previewAppInfo() {
+    ReclaimYourAttentionTheme {
+        appInfo("App Name", 60)
+    }
+}
+
+@Composable
+fun appInfo(appName: String, timeUsed: Int) {
+    Row {
+        Image(
+            painterResource(R.drawable.ic_launcher_foreground),
+            ""
+        )
+        Column {
+            Row {
+                Text(appName)
+                Text("$timeUsed min")
+            }
+            progressBar(30)
+        }
+
+    }
+}
+
+@Composable
+fun progressBar(percentage: Byte) {
+    Text(".......................$percentage%")
 }
